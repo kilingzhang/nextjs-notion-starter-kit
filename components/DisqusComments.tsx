ffce524c4d54ab88d791081242c3450c900d17ce
kilingzhang <i@kilingzhang.com>
2022-11-26 19:48:42 +0800

    fix # DisqusComment

---
+++ components/DisqusComments.tsx
@@ -0,0 +1,18 @@
+import { DiscussionEmbed } from 'disqus-react'
+import React from 'react'
+
+const DisqusComments = ({ post }) => {
+  if (post) {
+    console.log('post', post)
+    const disqusShortname = 'kilingzhang'
+    const disqusConfig = {
+      url: post.url,
+      identifier: post.id, // Single post id
+      title: post.title // Single post title
+    }
+    return <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+  }
+
+  return <></>
+}
+export default DisqusComments
